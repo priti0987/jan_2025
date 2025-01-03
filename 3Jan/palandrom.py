4ba4d93ff616b73520101e55ada78d0e091c6cc4
priti <pritibhushanf@gmail.com>
2025-01-03 15:47:13 +0530

    3jan2025

---
+++ 3Jan/palandrom.py
@@ -0,0 +1,13 @@
+#priti =itirp
+mystring = "nitin"
+# print(mystring==mystring[::-1])
+
+#string reverse
+revString=""
+for i in mystring:
+    revString =   i+revString
+if(mystring==revString):
+    print(f"Given string::{mystring} is palindrom")
+
+
+# print(revString)
