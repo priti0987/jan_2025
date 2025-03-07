f303bd63565ef243874d18436c1dcb560d20666a
priti <pritibhushanf@gmail.com>
2025-03-07 22:08:57 +0530

    7march2025

--- march/regularExpression.py
+++ march/regularExpression.py
@@ -1,1 +1,10 @@
-#regular expression 7 march 
+#regular expression 7 march
+import re
+text = '''
+Priti priya Ok '''
+
+pattern = r"[A-Z]"
+matchh = re.finditer(pattern,text)
+print(matchh)
+for i in matchh:
+    print(i)
