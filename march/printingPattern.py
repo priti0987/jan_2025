c9c802a6f46c7b6dffd6e59976cee5fadcfbe43b
priti <pritibhushanf@gmail.com>
2025-03-23 10:30:06 +0530

    23march2025

--- march/printingPattern.py
+++ march/printingPattern.py
@@ -1,3 +1,5 @@
+#test
+print("star pattern sums")
 #nexted loop
 # 1 star pattern ,
 '''
@@ -11,7 +13,38 @@
 for i in range(5):
     for i in range(5):
         print("*",end=" ")
-    print("")
+    print()
 
-#test
-print("star pattern sums")
+'''Print tringle increamental with left'''
+
+for i in range(6):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+
+'''Print tringle increamental with upsidedown'''
+
+for i in range(6,0,-1):
+    for j in range(i):
+        print("*",end=" ")
+    print()
+
+
+'''
+print
+1
+12
+123
+1234
+12345 with space '''
+
+for i in range(1,6):
+    for j in range(1,i+1):
+        print(j,end=" ")
+    print()
+
+
+    
+
+
