dd4e47063c7ec221641022225f35ee9d50bdedcd
priti <pritibhushanf@gmail.com>
2025-03-23 10:13:36 +0530

    23march2025

---
+++ march/printingPattern.py
@@ -0,0 +1,17 @@
+#nexted loop
+# 1 star pattern ,
+'''
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+* * * * *
+'''
+
+for i in range(5):
+    for i in range(5):
+        print("*",end=" ")
+    print("")
+
+#test
+print("star pattern sums")
