35f75caef26faaa09467ef0e39e7d4e12cc74dad
priti <pritibhushanf@gmail.com>
2025-04-22 23:33:39 +0530

    22april

--- april_2025/pythonlogic_Q_22_april.py
+++ april_2025/pythonlogic_Q_22_april.py
@@ -7,3 +7,11 @@ if inputString == inputString[::-1]:
     print(f"Given String {inputString} is palendrome")
 else:
     print(f"Given String {inputString} is not a palendrome")
+
+#print horizontally
+
+listl = [1, 3, 6, 7]
+for number in listl:
+    print(number, end= " ")
+# Output:
+# 13 6 7
