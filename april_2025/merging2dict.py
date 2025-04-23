5d953ac5ff9f91f6f810019c3b55b9d00048d681
priti <pritibhushanf@gmail.com>
2025-04-23 18:44:26 +0530

    22april

--- april_2025/merging2dict.py
+++ april_2025/merging2dict.py
@@ -1,5 +1,10 @@
-namel = {"kelly": 23,"Derick": 14, "John": 7}
-name2 = {"Ravi": 45, "Mpho": 67}
-#names = namel | name2
-names ={**namel,**name2}
-print(names)
+import calendar
+
+month = calendar.month(2025,4)
+print(month)
+
+# namel = {"kelly": 23,"Derick": 14, "John": 7}
+# name2 = {"Ravi": 45, "Mpho": 67}
+# #names = namel | name2
+# names ={**namel,**name2}
+# print(names)
