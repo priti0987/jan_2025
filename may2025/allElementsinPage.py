c9147d90213f11ac7c9c4fdc3f38a71de315d429
priti <pritibhushanf@gmail.com>
2025-05-07 21:30:37 +0530

    7may

--- may2025/allElementsinPage.py
+++ may2025/allElementsinPage.py
@@ -1,6 +1,9 @@
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
+
+from selenium.webdriver.support.wait import WebDriverWait
 
 # Start a browser session (make sure you have the correct WebDriver installed)
 driver = webdriver.Chrome()  # or Firefox, Edge, etc.
@@ -11,14 +14,42 @@ driver.maximize_window()
 time.sleep(3)
 
 # Find all elements using a universal selector '*'
-all_elements = driver.find_elements(By.CSS_SELECTOR, "*")
+# all_elements = driver.find_elements(By.CSS_SELECTOR, "*")
 
 # Print each element's tag name
-for element in all_elements:
-    try:
-        print(element.tag_name)
-    except Exception as e:
-        print("Error retrieving tag:", e)
+# for element in all_elements:
+#     try:
+#         print("tag of element ",element.tag_name)
+#     except Exception as e:
+#         print("Error retrieving tag:", e)
+
+def writeIn(by, desc,data):
+    wait = WebDriverWait(driver, 10)
+    by = by.upper()
+    if by == 'ID':
+        wait.until(EC.element_to_be_clickable((By.ID, desc)))
+        driver.find_element(By.ID,desc).send_keys(data)
+    elif by == 'XPATH':
+        wait.until(EC.element_to_be_clickable((By.XPATH, desc)))
+        driver.find_element(By.XPATH,desc).send_keys(data)
+    elif by=='Name':
+        wait.until(EC.element_to_be_clickable((By.NAME, desc)))
+        driver.find_element(By.NAME, desc).send_keys(data)
+
+
+def clickElement(by, desc):
+    wait = WebDriverWait(driver, 10)
+    by = by.upper()
+    if by == 'ID':
+        wait.until(EC.element_to_be_clickable((By.ID, desc)))
+        driver.find_element(By.ID,desc).click()
+    if by == 'XPATH':
+        wait.until(EC.element_to_be_clickable((By.XPATH, desc)))
+        driver.find_element(By.XPATH,desc).click()
+
+# clickElement('XPATH','(//a[contains(@href,"customer/login")])[2]')
+writeIn('XPATH','//input[@name="email"]','priti')
+time.sleep(3)
 
 # Close the browser
 driver.quit()
