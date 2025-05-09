22c26d0902b2ce2c98778ac475e4e492b581c14e
priti <pritibhushanf@gmail.com>
2025-05-09 23:18:18 +0530

    9may

--- may2025/recursionlimit.py
+++ may2025/recursionlimit.py
@@ -1,5 +1,24 @@
-import  sys
+# import  sys
+#
+# print(sys.getrecursionlimit())
+# sys.setrecursionlimit(100)
+# print(sys.getrecursionlimit())
 
-print(sys.getrecursionlimit())
-sys.setrecursionlimit(100)
-print(sys.getrecursionlimit())
+#Direct recurtion
+#
+# n = int(input("Enter n : "))
+#
+# def print_naturalNumber(n):
+#     print(n)
+#     if n==1:
+#         return
+#     return print_naturalNumber(n-1)
+#
+# print_naturalNumber(n)
+
+def fact(n):
+    if n==0:
+        return 1
+    return n*fact(n-1)
+
+print(fact(5))
