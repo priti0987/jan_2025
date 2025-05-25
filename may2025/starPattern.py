c89848bddee4e321b8f868353ffee6f336adf657
priti <pritibhushanf@gmail.com>
2025-05-25 17:59:29 +0530

    25may

--- may2025/starPattern.py
+++ may2025/starPattern.py
@@ -10,4 +10,17 @@ def pattern2(n):
             print("*",end="")
         print("")
 
-pattern2(6)
+
+def pattern3(n):
+    for i in range((2*n)):
+        if i>n:
+            c=(2*n)-i
+        else:
+            c=i
+        # for ii in range(n-c):
+        #     print(" ",end="")
+        for j in range(c):
+            print("*",end="")
+        print("")
+
+pattern3(7)
