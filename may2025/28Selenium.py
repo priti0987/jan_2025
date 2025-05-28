65e96147bf3ee32701549e204e31eb42a404ef91
priti <pritibhushanf@gmail.com>
2025-05-28 21:55:20 +0530

    28may

---
+++ may2025/28Selenium.py
@@ -0,0 +1,14 @@
+import keyword
+
+# print(keyword.kwlist)
+
+#exception handing
+#try except else
+
+print("start..")
+try:
+    print(False)
+except:
+    print("in exception")
+else:
+    print("else")
