91816bdc99a39bc66b552f301e5b555349f4dd1d
priti <pritibhushanf@gmail.com>
2025-05-29 22:48:09 +0530

    29may

---
+++ may2025/stingPrograms.py
@@ -0,0 +1,23 @@
+#  Reverse a string
+#............myString= "priti"
+#............print(myString[::-1])
+
+#  Check if a string is a palindrome
+def ispalindrome(sttring):
+    return (sttring == sttring[::-1])
+
+print(ispalindrome("madam"))
+
+#  Find duplicate characters in a string
+#  Count vowels, consonants, digits, and spaces
+#  Remove all whitespace from a string
+#  Find the first non-repeated character ( asked in nagarro recently )
+#  Check if two strings are anagrams
+#  Count occurrences of a character
+#  Convert string to integer (without using built-in methods)
+#  Check if one string contains another
+#  Replace all occurrences of a character
+#  Split a string by delimiter (e.g., comma, space)
+#  Sort characters in a string alphabetically
+#  Find the longest word in a sentence
+#  Compare two strings without using equals()
