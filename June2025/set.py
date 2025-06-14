0e01e6c3661125d3cf80b4aaa3a95e844104db1e
priti <pritibhushanf@gmail.com>
2025-06-14 18:40:06 +0530

    14june

--- June2025/set.py
+++ June2025/set.py
@@ -1,15 +1,24 @@
-myset = {2,4,5,2,7}
-print(type(myset))
-print(len(myset))
-myset.add(6)
-print(myset)
-print(len(myset))
-for i in myset:
-    print(i)
-myset.remove(2)
-print(myset)
-print(len(myset))
-myset.discard(4)
-print(myset)
-myset.clear()
-print(myset)
+# myset = {2,4,5,2,7}
+# print(type(myset))
+# print(len(myset))
+# myset.add(6)
+# print(myset)
+# print(len(myset))
+# for i in myset:
+#     print(i)
+# myset.remove(2)
+# print(myset)
+# print(len(myset))
+# myset.discard(4)
+# print(myset)
+# myset.clear()
+# print(myset)
+
+myset1 = {2,4,3,2,9,6,7}
+myset2={2,3,4,1,7}
+print("myset1 = ",myset1)
+print("myset2 = ",myset2)
+print("myset1|myset2 = union = ",myset1|myset2)
+print("myset1-myset2 = diff = ",myset1-myset2)
+print("myset1 & myset2 = symetric = ",myset1 & myset2)
+print("myset1^myset2 = Intersection = ",myset1^myset2)
