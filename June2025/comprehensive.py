2324b1afdb5936902dbc803443565dd007a3c5e5
priti <pritibhushanf@gmail.com>
2025-06-18 17:27:42 +0530

    18June1

--- June2025/comprehensive.py
+++ June2025/comprehensive.py
@@ -5,6 +5,19 @@ mylist = [n*2 for n in nums]
 even=[n for n in nums if n%2==0]
 #print(even)
 onlyOdd=[2*n-1 for n in nums]
-print(onlyOdd)
+#print(onlyOdd)
 podd = [n+1 for n in nums if n%2==0]
-print(podd)
+# print(podd)
+
+mylist = [(let,num) for let in 'abcd' for num in range(4)]
+# print(mylist)
+
+name = ['pandurange','pratibha','pallavi','priya','priti']
+role = ['father','mother','eldersister','twin sis1','twin sis2']
+# fam_1= [(zip(name,role))]
+# print((fam_1))
+myfDictionary = {name : role for name ,role in zip(name,role)}
+# print(myfDictionary)
+myset ={n for n in name}
+print(myset)
+print(type(myset))
