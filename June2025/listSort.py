57f3ad99e4297f7ab4e38283e629e0ddf979595a
priti <pritibhushanf@gmail.com>
2025-06-20 21:20:01 +0530

    20June1

--- June2025/listSort.py
+++ June2025/listSort.py
@@ -1,4 +1,11 @@
 listl = [2, 5, 6, 8, 1, 8, 9, 11]
-# listl.sort(reverse=True)
-# print(listl)
+#listl.sort()
+max = listl[0]
+print(max)
+for i in range(len(listl)):
+    if max <listl[i]:
+        max=listl[i]
+
+print(max)
+
 
