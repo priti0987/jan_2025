5c40528870c6e836b8351a3e378412158f8dc330
priti <pritibhushanf@gmail.com>
2025-07-01 23:31:56 +0530

    1July

--- June2025/array.py
+++ June2025/array.py
@@ -1,23 +1,36 @@
-# 26. Reverse an array in-place.
-myList= [1,3,1,5,89,56]
+# # 26. Reverse an array in-place.
+# myList= [1,3,1,5,89,56]
+#
+# print("Reverse an array in-place == ",myList[::-1])
+#
+# # 27. Find the largest and smallest element.
+# print("27. Find the largest and smallest element.","largest == ",max(myList),"smallest == ",min(myList))
+# # 28. Check for duplicates in an array
+# # 29. Remove duplicates — return only unique values.
+# unique = []
+# duplicate = []
+# for i in myList:
+#     if i not in unique:
+#         unique.append(i)
+#     else:
+#         duplicate.append(i)
+# print("duplicate == ",duplicate)
 
-print("Reverse an array in-place == ",myList[::-1])
 
-# 27. Find the largest and smallest element.
-print("27. Find the largest and smallest element.","largest == ",max(myList),"smallest == ",min(myList))
-# 28. Check for duplicates in an array
-unique = []
-duplicate = []
-for i in myList:
-    if i not in unique:
-        unique.append(i)
-    else:
-        duplicate.append(i)
-print("duplicate == ",duplicate)
-
-
-# 29. Remove duplicates — return only unique values.
 # 30. Find the missing number from 1 to N.
+def find_missing_number(arr, N):
+    # Sum of first N natural numbers
+    expected_sum = N * (N + 1) // 2
+    actual_sum = sum(arr)
+    return expected_sum - actual_sum
+
+# Example usage
+N = 10
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 10]  # 9 is missing
+missing = find_missing_number(arr, N)
+print(f"The missing number is: {missing}")
+
+
 # 31. Move all zeros to the end — keep order.
 # 32. Rotate the array left/right by K positions.
 # 33. Find the Kth largest/smallest element.
---
+++ June2025/star.py
@@ -0,0 +1,12 @@
+n=6
+
+for i in range(n):
+     print("*"*i)
+for i in range(n):
+     print("*"*(n-i))
+
+for i in range(n):
+     print(" "*i+"*"*(n-i))
+
+for i in range(n):
+     print(" "*(n-i)+"*"*i)
