d1d363caa3c757b5836e0162cb1c12fc36fdfe3a
priti <pritibhushanf@gmail.com>
2025-07-02 23:17:50 +0530

    2July

--- July2025/Day1.py
+++ July2025/Day1.py
@@ -1,6 +1,23 @@
 #Prints numbers from 1 to 10
-n = 10
-sum=0
-for i in range(1,n+1):
-    sum=sum+i
-print(sum)
+# n = 10
+# sum=0
+# for i in range(1,n+1):
+#     sum=sum+i
+# print(sum)
+#find the sum of numbers from 1 to n
+# n=int(input("Enter the n = "))
+# sum=0
+# for i in range(1,n+1):
+#     sum = sum + i
+# print(sum)
+#Check if number is ever or odd
+# n=30
+# if n % 2 ==0:
+#     print("Given number is Even")
+# else:
+#     print("Given number is odd")
+#Find maximum of three numbers
+myList= [5,60,9]
+print(max(myList))
+
+#swap two variables
