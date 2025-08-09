51630fd3a14aca9e062994c2995ef93a275221b6
priti <pritibhushanf@gmail.com>
2025-08-09 21:14:16 +0530

    9Aug

---
+++ Aug/9Aug/swapNumbers.py
@@ -0,0 +1,25 @@
+num1 = 100
+num2 = 200
+print("Before Swaping")
+print("num1 = ",num1)
+print("num2 = ",num2)
+#Using
+# num1,num2 = num2,num1
+# print("After Swaping")
+# print("num1 = ",num1)
+# print("num2 = ",num2)
+# Using temp variable
+# temp = num1
+# num1=num2
+# num2=temp
+# print("After Swaping using temp")
+# print("num1 = ",num1)
+# print("num2 = ",num2)
+#using addition and subtraction
+num1=num1+num2
+num2=num1-num2
+num1=num1-num2
+print("After Swaping using addition and subtraction")
+print("num1 = ",num1)
+print("num2 = ",num2)
+
--- Aug/9Aug/xpath.py
+++ Aug/9Aug/xpath.py
@@ -6,22 +6,36 @@ from selenium.webdriver.common.by import By
 #selenium4 ... updated on 13dec2024
 #https://www.saucedemo.com/
 driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
-url = "https://www.saucedemo.com/"
+# url = "https://www.saucedemo.com/"
+url = "https://www.hyrtutorials.com/p/add-padding-to-containers.html"
 driver.get(url)
 
 driver.maximize_window()
-driver.find_element(By.XPATH, "//input[@id='user-name']").send_keys("standard_user")
-driver.find_element(By.XPATH, "//input[@id='password']").send_keys("secret_sauce")
-driver.find_element(By.ID, "login-button").click()
+def entertext(xpath, data):
+     driver.find_element(By.XPATH,xpath).clear()
+     driver.find_element(By.XPATH, xpath).send_keys(data)
+
+entertext("//*[text()='First Name ']/following::input[1]","priti")
 time.sleep(2)
-#
-products_txt = driver.find_element(By.XPATH,"//span[contains(text(),'Products')]").is_displayed()
+entertext("//*[text()='Last Name']/preceding::input","bhavika")
+
+# entertext(emailXpath,text)
+
+# driver.find_element(By.XPATH, "//input[@id='user-name']").send_keys("standard_user")
+# driver.find_element(By.XPATH, "//input[@id='password']").send_keys("secret_sauce")
+# driver.find_element(By.ID, "login-button").click()
+time.sleep(2)
+# #
+# products_txt = driver.find_element(By.XPATH,"//span[contains(text(),'Products')]").is_displayed()
 # print(products_txt)
 # exp_title = 'OrangeHRM'
-if products_txt:
-     print("Login Test Pass")
-else:
-     print("Login Test Failed")
+# if products_txt:
+#      print("Login Test Pass")
+# else:
+#      print("Login Test Failed")
+
+
+
 # time.sleep(10)
 print("done....")
-driver.quit()
+# driver.quit()
