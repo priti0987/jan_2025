e0726e9057ce83edbf785b87607dbfc47a51a94d
priti <pritibhushanf@gmail.com>
2025-08-21 08:04:08 +0530

    21Aug

--- Aug/20Aug/largestNumber.py
+++ Aug/20Aug/largestNumber.py
@@ -16,3 +16,35 @@ a=5
 b=10
 c=15
 print(a>b, "b is larget")
+
+
+#print prime numbe rtill 20 in java
+# // Online Java Compiler
+# // Use this editor to write, compile and run your Java code online
+#
+# class Main {
+#     public static void main(String[] args) {
+#         //prime number : divid by same number and 1
+#         for (int i=1;i<=20;i++)
+#         {
+#             if(isPrime(i)){
+#                 System.out.println("Its a prime number "+i);
+#             }
+#         }
+#     }
+#     public static boolean isPrime(int Number){
+#         int count = 0;
+#         for (int i=1;i<=Number;i++)
+#         {
+#             if (Number %i ==0){
+#                 count++;
+#             }
+#         }
+#         if (count==2){
+#     //    System.out.println("Its a prime number");
+#         return true   ;
+#         }else {
+#             return false;
+#         }
+#
+#     }}
