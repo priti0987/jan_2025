092106144e9b39a3be76a4934e473f0a5bd606a0
priti <pritibhushanf@gmail.com>
2025-08-23 22:10:35 +0530

    23Aug

---
+++ Aug/23Aug/FindSubstring.py
@@ -0,0 +1,8 @@
+myString ="Welcome to python programmer"
+string1="python"
+
+if(myString.find(string1)==-1):
+    print("Not found")
+else:
+    print("found")
+
---
+++ Aug/23Aug/lenOfString.py
@@ -0,0 +1,8 @@
+#method 1 for loop
+
+myString = "Welcome"
+count = 0
+for i in myString:
+    count=count+1
+
+print(count)
