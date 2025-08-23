1e220a70e976fddaddce6678c92421f556fdd067
priti <pritibhushanf@gmail.com>
2025-08-23 22:22:43 +0530

    23Aug

--- Aug/23Aug/lenOfString.py
+++ Aug/23Aug/lenOfString.py
@@ -2,7 +2,19 @@
 
 myString = "Welcome"
 count = 0
-for i in myString:
-    count=count+1
+# for i in myString:
+#     count=count+1
+#
+# print(count)
 
-print(count)
+#method 2 : while and slicing
+# while myString[count :]:
+#     count=count+1
+# print(count)
+#method3 len()
+
+# print(len(myString))
+
+#method 4 :using join and count
+randomStr = "we"
+print(randomStr.join(myString).count(randomStr)+1)
