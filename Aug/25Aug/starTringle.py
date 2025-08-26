4ff3bf0ba1de1af0e4d8104d3b34cdca38c147d5
priti <pritibhushanf@gmail.com>
2025-08-26 16:18:39 +0530

    25Aug

---
+++ Aug/25Aug/starTringle.py
@@ -0,0 +1,17 @@
+n=6
+for i in range(n):
+    print("*"*i)
+    pass
+
+for i in range(n):
+    print("*"*(n-i))
+    pass
+
+for i in range(n):
+    pass
+    print(" "*(n-i)+"*"*i)
+
+for i in range(n):
+    pass
+    print(" "*i+"*"*(n-i))
+
