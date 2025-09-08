3e5264f33794b482a53f940b9a632a74e738fa1b
priti <pritibhushanf@gmail.com>
2025-09-08 22:44:55 +0530

    8sept

---
+++ PlaywrightAPI/PostRequest8Sept.py
@@ -0,0 +1,17 @@
+from playwright.sync_api import sync_playwright
+#need to use random function to create new email each time to get 201 response
+
+def test_list(playwright: sync_playwright()):
+    # random_email =
+    data = {
+        "name":"iooioku",
+        "gender":"male",
+        "email":"kokjj@email.kkk",
+        "status":"active"
+    }
+    myHeaders={"Authorization":"Bearer af5f99755b7300518fad5852a6b18429dbfb69eae3867303570965c2ee6fbaff"}
+    context = playwright.request.new_context(base_url="https://gorest.co.in/")
+    response = context.post(url = "public/v2/users",data=data,headers=myHeaders)
+    print(response)
+    assert response.status == 201
+    assert response.status_text == 'Created'
---
+++ sept/8Sept_1.py
@@ -0,0 +1,5 @@
+profile = {'name':'raju','age':100,'sal':2500}
+# myItems = profile.items()
+
+for i in profile.keys():
+    print(i)
