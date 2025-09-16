6357bbdcb96ca8c74e33505ab8a381453a8f197f
priti <pritibhushanf@gmail.com>
2025-09-16 08:58:39 +0530

    16Sept

---
+++ sept/16Sept.py
@@ -0,0 +1,19 @@
+#binary search
+
+def binarySearch(arr,target):
+    arr.sort()
+    low=0
+    high = len(arr)-1
+    print("high",high)
+    while low<high:
+        midIndex=(low+high)//2
+        print("midIndex",midIndex)
+        if arr[midIndex]==target:
+            return True
+        elif arr[midIndex]>target:
+            high =midIndex-1
+        elif arr[midIndex]<target:
+            low =midIndex+1
+    return False
+a=[6,66,8,9,22,111]
+print(binarySearch(a,6))
