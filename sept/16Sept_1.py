0db771cdf5c871433bf9be9b7ca5a6a114161d0b
priti <pritibhushanf@gmail.com>
2025-09-17 10:26:54 +0530

    17Sept

--- sept/16Sept_1.py
+++ sept/16Sept_1.py
@@ -6,5 +6,15 @@
 # #************************************************************
 #
 #String methods
-print("capitalize => priti".capitalize())
-print("caseFold => PriTi".casefold())
+# print("capitalize => priti".capitalize())
+# print("caseFold => PriTi".casefold())
+# print("center => priti".center(10))
+# print("count = > priti".count('t'))
+
+
+l=[1,2]
+print(l)
+(l.append(5))
+print(l)
+l.insert(2,3)
+print(l)
---
+++ sept/17sept.py
@@ -0,0 +1,16 @@
+#interger in decimal
+
+import decimal
+i=10.2
+iD = decimal.Decimal(i)
+print(type(i))
+print(iD)
+print(type(iD))
+
+#string to decimal
+
+pri = "456"
+print(type(pri))
+newI=decimal.Decimal(pri)
+print(newI)
+print(type(newI))
